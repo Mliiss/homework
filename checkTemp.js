faaee0990f025e779c1c64cdfa358348fee5f96a
Mliiss <92250116+Mliiss@users.noreply.github.com>
2021-10-14 16:26:04 +0300

    Create checkTemp.js

---
+++ checkTemp.js
@@ -0,0 +1,11 @@
+let checkTemp = prompt("Katla temperatuurivahemik");
+
+if (checkTemp >= 0 && checkTemp <= 20 ) {
+    console.log ("-1");
+} 
+if (checkTemp >= 21 && checkTemp <= 40 ) {
+    console.log ("0");
+} 
+if (checkTemp >= 41 && checkTemp <= 60 ) {
+    console.log ("1");
+}
