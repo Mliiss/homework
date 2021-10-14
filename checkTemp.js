dbb24e7611b6b0fc94ac8a0c31ec0d7df640c9c8
Mliiss <92250116+Mliiss@users.noreply.github.com>
2021-10-14 16:39:06 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,11 +1,8 @@
-let checkTemp = prompt("Katla temperatuurivahemik");
-
-if (checkTemp >= 0 && checkTemp <= 20 ) {
-    console.log ("-1");
-} 
-if (checkTemp >= 21 && checkTemp <= 40 ) {
-    console.log ("0");
-} 
-if (checkTemp >= 41 && checkTemp <= 60 ) {
-    console.log ("1");
-}
+function checkTemp(checkTemp) {
+    if (checkTemp >= 0 && checkTemp <= 20 ) {
+        console.log ("-1");
+    }  else if (checkTemp >= 21 && checkTemp <= 40 ) {
+        console.log ("0");
+    }  else (checkTemp >= 41 && checkTemp <= 60 ) 
+        console.log ("1");
+    }
