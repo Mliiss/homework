6c2f82dc53f005e5dd1f2c97296eacade3693aa5
Mliiss <92250116+Mliiss@users.noreply.github.com>
2021-10-14 16:39:35 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,8 +1,8 @@
 function checkTemp(checkTemp) {
     if (checkTemp >= 0 && checkTemp <= 20 ) {
         console.log ("-1");
-    }  else if (checkTemp >= 21 && checkTemp <= 40 ) {
+    }  else if (checkTemp <= 40 ) {
         console.log ("0");
-    }  else (checkTemp >= 41 && checkTemp <= 60 ) 
+    }  else (checkTemp <= 60 ) 
         console.log ("1");
     }
